06b03c2db47be0162110044964a34dff1b81a59d
Tsumarau, Alex: IT (PRG) <alex.tsumarau@barcap.com>
2016-04-20 17:14:17 +0200

    ComponentGraph - new version incomplete

--- codility/src/main/java/org/alext/dataStructures/disjointSets/ComponentGraph.java
+++ codility/src/main/java/org/alext/dataStructures/disjointSets/ComponentGraph.java
@@ -2,7 +2,6 @@ package org.alext.dataStructures.disjointSets;
 
 import java.io.InputStream;
 import java.util.*;
-import java.util.stream.Collectors;
 
 /**
  * Created by alex on 4/19/2016.
@@ -10,45 +9,36 @@ import java.util.stream.Collectors;
 public class ComponentGraph {
 
     public static String run(InputStream in) {
-
         Scanner sc = new Scanner(in);
         int N = sc.nextInt();
 
-        List<TreeSet<Integer>> sets = new ArrayList<>();
-
+        Map<Integer, Set<Integer>> fws = new HashMap<Integer, Set<Integer>>();
+        Map<Integer, Set<Integer>> bks = new HashMap<Integer, Set<Integer>>();
 
         for (int index = 0; index < N; index++) {
-            TreeSet<Integer> newSet = new TreeSet<>();
-            newSet.add(sc.nextInt());
-            newSet.add(sc.nextInt());
-            sets.add(newSet);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            addRib(a, b, fws);
+            addRib(b, a, bks);
         }
 
-        long emptySets = 0;
-        do {
-            for (int i = 0; i < sets.size(); i++) {
-                for (int j = i + 1; j < sets.size(); j++) {
-                    TreeSet<Integer> donor = sets.get(i);
-                    TreeSet<Integer> acceptor = sets.get(j);
-
-                    for (Integer dn : donor) {
-                        if (acceptor.contains(dn)) {
-                            acceptor.addAll(donor);
-                            donor.clear();
-                            break;
-                        }
-                    }
-                }
-            }
-            emptySets = sets.stream().filter(s -> s.isEmpty()).count();
-            sets = sets.stream().filter(s -> !s.isEmpty()).collect(Collectors.toList());
+        if(){
         }
-        while (emptySets > 0);
 
 
-        Optional<Integer> min = sets.stream().map(s -> s.size()).reduce(Math::min);
-        Optional<Integer> max = sets.stream().map(s -> s.size()).reduce(Math::max);
-
-        return min.get() + " " + max.get();
+        /*Optional<Integer> min = partlyFlattenSet.stream().map(s -> s.size()).reduce(Math::min);
+        Optional<Integer> max = partlyFlattenSet.stream().map(s -> s.size()).reduce(Math::max);
+        return min.get() + " " + max.get();*/
     }
+
+    private static void addRib(int start, int end, Map<Integer, Set<Integer>> links) {
+        if (links.containsKey(start)) {
+            links.get(start).add(end);
+        } else {
+            Set<Integer> map = new HashSet<>();
+            map.add(end);
+            links.put(start, map);
+        }
+    }
+
 }
