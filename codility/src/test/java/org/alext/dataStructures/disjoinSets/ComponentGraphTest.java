0bb9eaa60ef0adfdeae2f3a356d4816ffbad2e0a
Tsumarau, Alex: IT (PRG) <alex.tsumarau@barcap.com>
2016-05-09 14:45:47 +0200

    ComponentGraph long test

--- codility/src/test/java/org/alext/dataStructures/disjoinSets/ComponentGraphTest.java
+++ codility/src/test/java/org/alext/dataStructures/disjoinSets/ComponentGraphTest.java
@@ -2,7 +2,10 @@ package org.alext.dataStructures.disjoinSets;
 
 import org.alext.dataStructures.disjointSets.ComponentGraph;
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,6 +16,7 @@ public class ComponentGraphTest {
 
     @Test
     public void testRun() {
+        long millisStart = System.currentTimeMillis();
         String input = "5\n" +
                 "1 6 \n" +
                 "2 7\n" +
@@ -24,10 +28,21 @@ public class ComponentGraphTest {
         String out = graph.run(IOUtils.toInputStream(input));
 
         assertEquals("2 4", out);
+
+        System.out.println("Done in(ms): "+(System.currentTimeMillis()-millisStart));
     }
 
     @Test
     public void testCase25() {
-        String expectedOut = "2 1072";
+        long millisStart = System.currentTimeMillis();
+
+
+        InputStream in = this.getClass().getResourceAsStream("/org.alext/dataStructures/disjoinSets/case25.txt");
+        ComponentGraph graph = new ComponentGraph();
+        String out = graph.run(in);
+
+        Assert.assertEquals("2 1072", out);
+
+        System.out.println("Done in(ms): "+(System.currentTimeMillis()-millisStart));
     }
 }
