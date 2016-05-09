43fed273bf6317149006c059da8ca529f7c6a102
Tsumarau, Alex: IT (PRG) <alex.tsumarau@barcap.com>
2016-05-09 11:40:41 +0200

    ComponentGraph - new version - perfomance problems

--- codility/src/main/java/org/alext/dataStructures/disjointSets/ComponentGraph.java
+++ codility/src/main/java/org/alext/dataStructures/disjointSets/ComponentGraph.java
@@ -11,34 +11,48 @@ public class ComponentGraph {
     public static String run(InputStream in) {
         Scanner sc = new Scanner(in);
         int N = sc.nextInt();
-
-        Map<Integer, Set<Integer>> fws = new HashMap<Integer, Set<Integer>>();
-        Map<Integer, Set<Integer>> bks = new HashMap<Integer, Set<Integer>>();
+        List<TreeSet<Integer>> sets = new ArrayList<>();
 
         for (int index = 0; index < N; index++) {
             int a = sc.nextInt();
             int b = sc.nextInt();
-            addRib(a, b, fws);
-            addRib(b, a, bks);
+
+            addRib(a, b, sets);
         }
 
-        if(){
-        }
-
-
-        /*Optional<Integer> min = partlyFlattenSet.stream().map(s -> s.size()).reduce(Math::min);
-        Optional<Integer> max = partlyFlattenSet.stream().map(s -> s.size()).reduce(Math::max);
-        return min.get() + " " + max.get();*/
+        Optional<Integer> min = sets.stream().map(s -> s.size()).reduce(Math::min);
+        Optional<Integer> max = sets.stream().map(s -> s.size()).reduce(Math::max);
+        return min.get() + " " + max.get();
     }
 
-    private static void addRib(int start, int end, Map<Integer, Set<Integer>> links) {
-        if (links.containsKey(start)) {
-            links.get(start).add(end);
-        } else {
-            Set<Integer> map = new HashSet<>();
-            map.add(end);
-            links.put(start, map);
+    private static void addRib(Integer a, Integer b, List<TreeSet<Integer>> sets) {
+        TreeSet<Integer> firstHolder = null;
+        List<TreeSet> toRemove = new ArrayList<>();
+
+        for (int index = 0; index < sets.size(); index++) {
+            TreeSet<Integer> set = sets.get(index);
+            if (set.contains(a) || set.contains(b)) {
+                set.add(a);
+                set.add(b);
+
+                if (firstHolder != null) {
+                    firstHolder.addAll(set);
+                    set.clear();
+                    toRemove.add(set);
+                } else {
+                    firstHolder = set;
+                }
+            }
         }
+
+        if (firstHolder == null) {
+            TreeSet<Integer> newSet = new TreeSet<>();
+            newSet.add(a);
+            newSet.add(b);
+            sets.add(newSet);
+        }
+
+        sets.removeAll(toRemove);
     }
 
 }
