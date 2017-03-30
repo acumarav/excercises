a82f71b7a3963da91b778e94fa40ecfe4a20e931
alex cumarev <alex.cumarev@distcotech.com>
2017-03-30 15:47:37 +0200

    no working aes decryption

--- codility/src/main/java/org/alext/threads/HangingThread.java
+++ codility/src/main/java/org/alext/threads/HangingThread.java
@@ -1,8 +1,11 @@
 package org.alext.threads;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,12 +15,60 @@ public class HangingThread {
 
     private static Object LOCK = new Object();
 
-    public int runCalculations(int scale) {
+    private ForkJoinPool executors = ForkJoinPool.commonPool();
+
+
+    public int runFutureCalculations(int scale) {
+        //ExecutorService executors = Executors.newCachedThreadPool();
+
+        List<MyTask> tasks = new ArrayList<>();
+        for (int index = 0; index < scale; index++) {
+
+            MyTask task = new MyTask(index);
+            tasks.add(task);
+        }
+
+        return processFutures(tasks);
+    }
+
+    private int processFutures(List<MyTask> futures) {
+
+        System.out.println("processing: " + futures.size());
+
+        futures.forEach(fut -> executors.execute(fut));
+
+        List<MyTask> failed = new ArrayList<>();
+
+        int sum = 0;
+        for (MyTask task : futures) {
+
+            try {
+                int val = task.get(20, TimeUnit.MILLISECONDS);
+                sum += val;
+
+            } catch (Throwable e) {
+                task.cancel(true);
+                failed.add(task.cloneTask());
+            }
+        }
+
+        if (failed.isEmpty()) {
+            return sum;
+        } else {
+            return sum + processFutures(failed);
+        }
+    }
+
+    /*public int runCalculations(int scale) {
 
         List<CompletableFuture> futures = new ArrayList<>();
         for (int index = 0; index < scale; index++) {
             final int finalIndex = index;
-            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> this.calculate(finalIndex));
+            CompletableFuture<Integer> future = CompletableFuture.supplyAsync(() -> this.calculate(finalIndex)).exceptionally(er -> {
+                System.out.println("Thread interruption");
+                Thread.currentThread().interrupt();
+                return -1;
+            });
 
             futures.add(future);
         }
@@ -27,24 +78,35 @@ public class HangingThread {
         int sum = 0;
         for (CompletableFuture<Integer> future : futures) {
             try {
-                int val = future.get(1000, TimeUnit.MILLISECONDS);
+                int val = future.get(100, TimeUnit.MILLISECONDS);
                 sum += val;
 
-            } catch (Exception e) {
-                future.completeExceptionally(new InterruptedException());
+            } catch (Throwable e) {
+
+
+                future.cancel(true);
+                future.join();
+
+                echoPool();
             }
         }
 
         return sum;
+    }*/
+
+    private static void echoPool() {
+        System.out.println("Pool State: " + ForkJoinPool.commonPool().toString());
     }
 
 
-    private int calculate(int value) {
+    private static int calculate(int value) {
         try {
             int pause = (int) Math.random() * 10;
             Thread.sleep(pause * 100);
 
-            if (value == 13) {
+            double rnd = Math.random() * 10;
+
+            if (rnd >= 6.0) {
                 System.out.println(Thread.currentThread().getName() + "- try to acquire LOCK");
                 synchronized (LOCK) {
                     System.out.println("LOCK acquired by: " + Thread.currentThread().getName());
@@ -54,10 +116,27 @@ public class HangingThread {
             }
 
             return value;
-        } catch (InterruptedException ex) {
+        } catch (Exception ex) {
             System.out.println(Thread.currentThread().getName() + " thread was interrupted");
             return -1;
         }
     }
 
+
+    private class MyTask extends FutureTask<Integer> {
+
+        private final int value;
+
+        MyTask(int val) {
+            super(() -> HangingThread.calculate(val));
+            this.value = val;
+        }
+
+        private MyTask cloneTask() {
+            return new MyTask(this.value);
+        }
+    }
+
 }
+
+
--- codility/src/test/java/org/alext/threads/HangingThreadTest.java
+++ codility/src/test/java/org/alext/threads/HangingThreadTest.java
@@ -4,6 +4,10 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+
 /**
  * Created by alext on 3/30/2017.
  */
@@ -18,7 +22,7 @@ public class HangingThreadTest {
 
     @Test
     public void test10(){
-        int result = command.runCalculations(10);
+        int result = command.runFutureCalculations(10);
 
         Assert.assertEquals(45,result);
     }
@@ -26,10 +30,44 @@ public class HangingThreadTest {
 
     @Test
     public void test20(){
-        int result = command.runCalculations(20);
+        int result = command.runFutureCalculations(20);
 
         Assert.assertEquals(190,result);
     }
 
+
+    @Test
+    public void test100(){
+        int result = command.runFutureCalculations(100);
+
+        Assert.assertEquals(4950,result);
+    }
+
+    @Test
+    public void test40PoolRecovery(){
+        int result = command.runFutureCalculations(40);
+        System.out.println("Result 1: "+result);
+
+        int result2 = command.runFutureCalculations(40);        ;
+
+        Assert.assertEquals(result,result2);
+
+        Assert.assertEquals(780,result);
+        Assert.assertEquals(780,result2);
+    }
+
+    @Test
+    public void test1000poolRecovery(){
+        int result = command.runFutureCalculations(100);
+        System.out.println("Result 1: "+result);
+
+        int result2 = command.runFutureCalculations(100);
+        System.out.println("Result 2: "+result2);
+
+
+        Assert.assertEquals(result,result2);
+        Assert.assertEquals(4950,result);
+        Assert.assertEquals(4950,result2);
+    }
 
 }
